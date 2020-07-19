de22c9fd02cee0d66ce0396702a339cfa88c7e68
Rogers <niwarogons@gmail.com>
2020-07-19 05:59:16 +0300

    This is the initial commit, I have set up nodejs for the app

---
+++ server.js
@@ -0,0 +1,17 @@
+//configuring the express web app
+const express = require('express');
+const app = express();
+//configuring nodemon that will automatically resart the server
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/Index.html");
+})
+
+//routing pages that dont exist on the application
+app.get('*', (req, res) => {
+  res.send('Error. This page doesnt exist on this planet');
+})
+
+//routing to the files in the controller
+
+// Making the clients listen to port 4000
+app.listen (4000, ()=> console.log("The application is running on port 4000"));
