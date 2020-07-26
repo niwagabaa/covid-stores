a16b5181eccbe86652c42dba41c609e0f1d1c143
Rogers <niwarogons@gmail.com>
2020-07-26 08:00:08 +0300

    Installed middle ware and pug that is going to be used in the application

--- server.js
+++ server.js
@@ -1,7 +1,45 @@
+// configuring DEPRNDENCIES
 //configuring the express web app
 const express = require('express');
+//configuring middleware
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan'):
+
+
+// INSTANTIANTIONS
 const app = express();
 //configuring nodemon that will automatically resart the server
+
+//configuring pug as a template engine in a views folder
+app.set('views engine', 'pug');
+app.set('views', './views');
+
+// MIDDLEWARE
+// To parse URL encoded data
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// To parse json data
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+
+// session middleware
+app.use(
+  session({
+    secret: 'arbitary-string'
+    resave: false,
+    saveUninitialised: true,
+    cookie: { secure: true }
+  })
+);
+
+app.use(logger('common'));
+
+//middleware for static files
+app.use(express.static('public'));
+
+//ROUTES
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/index.html");
 })
@@ -13,5 +51,6 @@ app.get('*', (req, res) => {
 
 //routing to the files in the controller
 
+// BOOTSTRAPPING THE SERVER
 // Making the clients listen to port 4000
 app.listen (4000, ()=> console.log("The application is running on port 4000"));
