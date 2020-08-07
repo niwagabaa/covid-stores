c148f4fc48906210ae6c0a8ae6ee3497fd50b16f
Rogers <niwarogons@gmail.com>
2020-08-07 18:26:51 +0300

    commented some unnecessary things from style.css and code too from server.js

--- server.js
+++ server.js
@@ -19,19 +19,14 @@ const mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017/covidstore';
 const path = require('path');
 
-
 app.use(express.static('public'));
 
 //configuring pug as a template engine in a views folder
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-//app.set('views', __dirname + '/views');
 
-// MIDDLEWARE
-//requiring mongose and connecting to db
-
-//requiring mongose and connecting to db
+//requiring mongoose and connecting to db
 mongoose.connect(url, {
 useNewUrlParser: true,
 useUnifiedTopology: true
@@ -42,37 +37,6 @@ console.log('Mongoose connection open');
 .on('error', (err) => {
 console.log(`Connection error: ${err.message}`);
 });
-
-
-
-
-
-
-
-//connect to mongoose
-// const dbPath = 'mongodb://localhost/firstrest';
-// const options = {useNewUrlParser: true, useUnifiedTopology: true}
-// const mongo = mongoose.connect(dbPath, options);
-// mongo.then(() => {
-//     console.log('connected');
-// }, error => {
-//     console.log(error, 'error');
-// })
-
-// // session middleware
-// app.use(
-//   session({
-//     secret: 'arbitary-string'
-//     resave: false,
-//     saveUninitialised: true,
-//     cookie: { secure: true }
-//   })
-// );
-//
-// app.use(logger('common'));
-
-//middleware for static files
-
 
 
 //ROUTES
@@ -100,21 +64,8 @@ const products = require('./routes/products')
 app.use('/products', products);
 
 
-// app.get("/register", (req, res) => {
-// res.sendFile('agentRegister.html', { root: path.join(__dirname, '../public') });
-//  });
-//
-// app.post('/register', (req, res) => {
-//   console.log(req.body);
-//   res.sendFile('agentRegister.html', { root: path.join(__dirname, '../public') });
-// })
-// //routing pages that dont exist on the application
-// app.get('*', (req, res) => {
-//   res.send('Error. This page doesnt exist on this planet');
-//})
-
-//routing to the files in the controller
-
+app.get('*', (req, res) => {
+res.send('Error. This page doesnt exist on this planet');
+})
 // BOOTSTRAPPING THE SERVER
-// Making the clients listen to port 4000
 app.listen (4000, ()=> console.log("The application is running on port 4000"));
